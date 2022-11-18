57d0e246e2e3562b08ae3892c3122c89fd7b8c83
Leonardo <l.guedes@ges.inatel.br>
2022-11-18 15:30:32 -0300

    qualquer coisa

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,10 +6,25 @@ document.addEventListener('keydown', function (event) {
     //🔊 SOUNDS
     var meow = new Audio("./assets/audios/meow.wav");
 
+    var meoww = new Audio("./assets/audios/laught.mp3");
+
+    var meowww = new Audio("./assets/audios/killbill.mp3");
+
+    var meowwww = new Audio("./assets/audios/Reggae_Horn.mp3");
+
     //⌨️ KEY EVENTS
     switch (event.keyCode) {
         case 32:
             meow.play();
+            break;
+        case 65:
+            meoww.play();
+            break;
+        case 66:
+            meowww.play();
+            break;
+        case 67:
+            meowwww.play();
             break;
     }
    
