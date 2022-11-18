cc7d5dfc8c10a52b0a5922dc1bb351e9ec4e5a44
Pablo Gaudard <pablogaudard@gmail.com>
2022-11-18 16:06:45 -0300

    Update files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,29 +4,44 @@ var laught = new Audio("./assets/audios/laught.mp3");
 var killbill = new Audio("./assets/audios/killbill.mp3");
 var Reggae_Horn = new Audio("./assets/audios/Reggae_Horn.mp3");
 
-document.addEventListener('keydown', function (event) {
+var interval, audioDuration;
 
-    console.log(event.keyCode);
+function playSound(keyCode) {
+
+    if (typeof keyCode === "object")
+        keyCode = keyCode.keyCode;
 
     //⌨️ KEY EVENTS
-    switch (event.keyCode) {
+    switch (keyCode) {
         case 32:
             meow.play();
+            audioDuration = meow.duration;
             break;
         case 65:
             laught.play();
+            audioDuration = laught.duration
             break;
         case 66:
             killbill.play();
+            audioDuration = killbill.duration;
             break;
         case 67:
             Reggae_Horn.play();
+            audioDuration = Reggae_Horn.duration;
             break;
     }
 
-    if ([32, 65, 66, 67].indexOf(event.keyCode) > -1) {
+    if ([32, 65, 66, 67].indexOf(keyCode) > -1) {
         catvibing.style.opacity = "1";
         catpiano.style.opacity = "1";
     }
 
-})
+    clearInterval(interval);
+    interval = setInterval(() => {
+        catvibing.style.opacity = "0";
+        catpiano.style.opacity = "0";
+    }, audioDuration * 1000);
+
+}
+
+document.addEventListener('keydown', playSound)
